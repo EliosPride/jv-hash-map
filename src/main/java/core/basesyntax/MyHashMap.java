140dd2083b0a24cbe586adf0b398c19be99a65fb
Illia Sydorchuk <batonvagon@gmail.com>
2022-05-13 14:47:46 +0300

    Initial commit

---
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -0,0 +1,26 @@
+package core.basesyntax;
+
+import java.util.Objects;
+
+/**
+ * <p>Реалізувати свою HashMap, а саме методи `put(K key, V value)`, `getValue()` та `getSize()`.
+ * Дотриматися основних вимог щодо реалізації мапи (initial capacity, load factor, resize...)
+ * За бажанням можна реалізувати інші методи інтрефейсу Map.</p>
+ */
+public class MyHashMap<K, V> implements MyMap<K, V> {
+
+    @Override
+    public void put(K key, V value) {
+        
+    }
+
+    @Override
+    public V getValue(K key) {
+        return null;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+}
